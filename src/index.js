94468d706c0228c929c5792aba6bb91fc292f168
Alexander Hed <alexander.hed@gmail.com>
2022-02-14 11:29:14 +0100

    button of dialog can now read value and output to console

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ import {displayController} from './display';
 import './style.css';
 
 const addTodoButton = document.getElementById("addTodo");
+const confirmAddTodoButton = document.getElementById('confirmButton');
 const addTodoDialog = document.getElementById('todoDialog');
+const todoInputValue = document.getElementById('todoTitleInput');
+
 
 addTodoButton.addEventListener('click', function onOpen() {
     if (typeof addTodoDialog.showModal === "function") {
-        //addTodoDialog.showModal();
-        let note = todo("title", "A new description", "date", "high");
-        todoList.addTodo(note);
-        displayController.addTodo(note);
+        addTodoDialog.showModal();
+        //let note = todo("title", "A new description", "date", "high");
+      //  todoList.addTodo(note);
+       // displayController.addTodo(note);
     } else {
       alert("The <dialog> API is not supported by this browser");
     }
+});
+
+confirmAddTodoButton.addEventListener('click', () => {
+    console.log(todoInputValue.value)
 });
 
 const newNote = todo("title", "A description again", "date", "high");
@@ -22,9 +29,5 @@ todoList.addTodo(newNote);
 const newNote2 = todo("title", "A description once again again", "date", "high");
 todoList.addTodo(newNote2);
 
-console.log(note.getDescription());
-console.log(note.getTitle());
-console.log(todoList.getTodoList());
-console.log(todoList.getTodoList().length);
 
 
